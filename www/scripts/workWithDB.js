abc064a9ee86dad30192932dd76a16abfa25bbb0
storm1196 <storm1196@gmail.com>
2013-04-02 17:39:59 +0300

    third commit

--- www/scripts/workWithDB.js
+++ www/scripts/workWithDB.js
@@ -51,7 +51,7 @@ function GetInformationFromSearchQueriesTable(db)
 {
 	db.transaction(
 	//function sql statements
-	function (tx){alert
+	function (tx){
 		ensureTableSearchQueriesExists(tx);
 		tx.executeSql('SELECT * FROM SearchQueries', 
 		[], 
